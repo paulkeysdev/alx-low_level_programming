3baeb11da417487b76cdd59cd2cff2fe861377a7
paulkeysdev <wainainapaul6880@gmail.com>
2022-09-19 15:32:29 -0700

    task sita

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,8 @@
 void puts2(char *str)
 {	int i = 0;
 	while (*(str + i) != '\0')
-	{	if (i % 2 == 0)
-			putchar(*(str + i));
+		if (i % 2 == 0)
+	{		putchar(*(str + i));
 		i++;
 	}
 	putchar(10);
