070d925049ffaa923431aaea18c1f2362bbac15b
paulkeysdev <wainainapaul6880@gmail.com>
2022-09-20 01:50:17 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,16 +2,19 @@
 #include "main.h"
 
 /**
-*puts2 - prints every other character
-* @str: string
+ * puts2 - prints every other character
+ * @str: string
  *
-*Return: nothing
-*/
+ * Return: nothing
+ */
 
 void puts2(char *str)
-{	int i = 0;
+{
+	int i = 0;
+
 	while (*(str + i) != '\0')
-	{	if (i % 2 == 0)
+	{
+		if (i % 2 == 0)
 			putchar(*(str + i));
 		i++;
 	}
