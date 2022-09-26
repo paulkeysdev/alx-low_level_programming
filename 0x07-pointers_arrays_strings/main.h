544a26ae1b7a01a985d00760fe324767c7e4ce51
paulkeysdev <wainainapaul6880@gmail.com>
2022-09-26 12:23:31 -0700

    This is my initial commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
-#define MAIN.h
+#define MAIN_H
 
 /**
  * void prototypes()
  * int prototypes()
  */
 
-int_putchar(char);
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
