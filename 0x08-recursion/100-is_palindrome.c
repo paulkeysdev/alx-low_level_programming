e5f90cff4ff715ad0b01b3e5fad27aff1aedaf7d
paulkeysdev <wainainapaul6880@gmail.com>
2022-09-29 12:45:51 -0700

    100-is_palindrome.cmodified

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,11 @@
+#include "main.h"
+
+/**
+ * is_palindrome - returns the 1 if s is a palindrome
+ * @s: string to be checked
+ *
+ * Return: 1 if s is a palindrome, 0 otherwise
+ */
 int is_palindrome(char *s)
 {
 	int flag = 1;
